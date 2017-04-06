b36a763428f5e22e2a1339d4449246ce6a363d94
Ivo <ivailo.hinov@gmail.com>
2017-04-06 16:20:14 +0100

    Game Ready For Hand-In

--- src/Game.java
+++ src/Game.java
@@ -34,7 +34,7 @@ public class Game extends GameCore implements MouseListener, MouseWheelListener,
     private boolean bossFight = false;
     private boolean gameWon = false;
     
-    //Pressed Key flags
+    // Pressed Key flags
     private boolean leftKey = false;
     private boolean rightKey = false;
     private boolean jumpKey = false;
@@ -56,6 +56,7 @@ public class Game extends GameCore implements MouseListener, MouseWheelListener,
     private Image mainMenu;
     private Image difficultyMenu;
     private Image fgImage = null;
+    private Sound levelMusic;
     
 	private Level currLevel;
     private Collision collider;
@@ -617,8 +618,8 @@ public class Game extends GameCore implements MouseListener, MouseWheelListener,
         mainMenu = loadImage("assets/images/Menus/mainMenu.png");
         difficultyMenu = loadImage("assets/images/Menus/diffMenu.png");
         fgImage = loadImage("assets/maps/Foregrounds/poles_front.png");
-        //levelMusic = new Sound("assets/sounds/level.wav"); TODO UNCOMMENT LATER ON
-        //levelMusic.start();
+        levelMusic = new Sound("assets/sounds/level.wav");
+        levelMusic.start();
 	}
     
 	/**
